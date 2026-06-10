10db39ab887e6a7e7cdb39bba1e65335b57b838c
kqnade <git@k4na.de>
2026-06-10 16:55:09 +0900

    ✨ feat: add Emacs basic commands (C-f, C-b, C-n, C-p, C-a, C-e, C-k)

--- src/ecro/core.clj
+++ src/ecro/core.clj
@@ -1,1 +1,81 @@
-(ns ecro.core)
+(ns ecro.core
+  (:require [ecro.buffer :as b]))
+
+;; Emacs basic commands
+
+(defn forward-char
+  "Move point forward one character (C-f)."
+  [buf]
+  (b/move-point-forward buf))
+
+(defn backward-char
+  "Move point backward one character (C-b)."
+  [buf]
+  (b/move-point-backward buf))
+
+(defn next-line
+  "Move point to the next line (C-n)."
+  [buf]
+  (let [text (:text buf)
+        point (:point buf)
+        current-line-col (b/point-to-line-column buf point)
+        current-line (first current-line-col)
+        current-col (second current-line-col)
+        lines (clojure.string/split text #"\n" -1)
+        next-line-idx (inc current-line)]
+    (if (< next-line-idx (count lines))
+      (let [next-line-text (nth lines next-line-idx)
+            target-col (min current-col (count next-line-text))
+            new-point (reduce + (map (comp inc count) (take next-line-idx lines)))]
+        (assoc buf :point (+ new-point target-col)))
+      buf)))
+
+(defn previous-line
+  "Move point to the previous line (C-p)."
+  [buf]
+  (let [text (:text buf)
+        point (:point buf)
+        current-line-col (b/point-to-line-column buf point)
+        current-line (first current-line-col)
+        current-col (second current-line-col)
+        lines (clojure.string/split text #"\n" -1)]
+    (if (> current-line 0)
+      (let [prev-line-idx (dec current-line)
+            prev-line-text (nth lines prev-line-idx)
+            target-col (min current-col (count prev-line-text))
+            new-point (reduce + (map (comp inc count) (take prev-line-idx lines)))]
+        (assoc buf :point (+ new-point target-col)))
+      buf)))
+
+(defn move-beginning-of-line
+  "Move point to beginning of current line (C-a)."
+  [buf]
+  (let [point (:point buf)
+        text (:text buf)
+        lines (clojure.string/split text #"\n" -1)
+        line-col (b/point-to-line-column buf point)
+        line-idx (first line-col)]
+    (assoc buf :point (reduce + (map inc (take line-idx lines))))))
+
+(defn move-end-of-line
+  "Move point to end of current line (C-e)."
+  [buf]
+  (let [point (:point buf)
+        text (:text buf)
+        lines (clojure.string/split text #"\n" -1)
+        line-col (b/point-to-line-column buf point)
+        line-idx (first line-col)
+        line-text (nth lines line-idx)]
+    (assoc buf :point (+ (reduce + (map inc (take line-idx lines))) (count line-text)))))
+
+(defn kill-line
+  "Kill from point to end of line (C-k)."
+  [buf]
+  (let [point (:point buf)
+        text (:text buf)
+        lines (clojure.string/split text #"\n" -1)
+        line-col (b/point-to-line-column buf point)
+        line-idx (first line-col)
+        col (second line-col)
+        line-text (nth lines line-idx)]
+    (assoc buf :text (str (subs text 0 point) (subs text (+ point (- (count line-text) col)))))))
--- test/ecro/core_test.clj
+++ test/ecro/core_test.clj
@@ -1,6 +1,7 @@
 (ns ecro.core-test
   (:require [clojure.test :refer :all]
-            [ecro.core :as core]))
+            [ecro.core :as core]
+            [ecro.buffer :as b]))
 
 (deftest test-jna-available
   (testing "JNA is available on classpath"
@@ -13,3 +14,83 @@
 (deftest test-core-namespace-loads
   (testing "ecro.core namespace loads without errors"
     (is (find-ns 'ecro.core))))
+
+(deftest test-forward-char
+  (testing "C-f moves point forward"
+    (let [buf (-> (b/make-buffer "test")
+                  (b/insert-char \a)
+                  (b/move-point-backward)
+                  (core/forward-char))]
+      (is (= 1 (:point buf))))))
+
+(deftest test-forward-char-at-end
+  (testing "C-f at end of buffer stays at end"
+    (let [buf (-> (b/make-buffer "test")
+                  (b/insert-char \a)
+                  (core/forward-char))]
+      (is (= 1 (:point buf))))))
+
+(deftest test-backward-char
+  (testing "C-b moves point backward"
+    (let [buf (-> (b/make-buffer "test")
+                  (b/insert-char \a)
+                  (b/insert-char \b)
+                  (core/backward-char))]
+      (is (= 1 (:point buf))))))
+
+(deftest test-backward-char-at-beginning
+  (testing "C-b at beginning stays at beginning"
+    (let [buf (-> (b/make-buffer "test")
+                  (core/backward-char))]
+      (is (= 0 (:point buf))))))
+
+(deftest test-next-line
+  (testing "C-n moves to next line"
+    (let [buf (-> (b/make-buffer "test")
+                  (b/insert-char \a)
+                  (b/insert-char \newline)
+                  (b/insert-char \b)
+                  (b/insert-char \newline)
+                  (b/insert-char \c)
+                  (b/move-point-backward)
+                  (b/move-point-backward)
+                  (b/move-point-backward)
+                  (core/next-line))]
+      (is (= 4 (:point buf))))))
+
+(deftest test-previous-line
+  (testing "C-p moves to previous line"
+    (let [buf (-> (b/make-buffer "test")
+                  (b/insert-char \a)
+                  (b/insert-char \newline)
+                  (b/insert-char \b)
+                  (core/next-line)
+                  (core/previous-line))]
+      (is (= 1 (:point buf))))))
+
+(deftest test-move-beginning-of-line
+  (testing "C-a moves to beginning of line"
+    (let [buf (-> (b/make-buffer "test")
+                  (b/insert-char \a)
+                  (b/insert-char \b)
+                  (core/move-beginning-of-line))]
+      (is (= 0 (:point buf))))))
+
+(deftest test-move-end-of-line
+  (testing "C-e moves to end of line"
+    (let [buf (-> (b/make-buffer "test")
+                  (b/insert-char \a)
+                  (b/insert-char \b)
+                  (b/move-point-backward)
+                  (core/move-end-of-line))]
+      (is (= 2 (:point buf))))))
+
+(deftest test-kill-line
+  (testing "C-k kills from point to end of line"
+    (let [buf (-> (b/make-buffer "test")
+                  (b/insert-char \a)
+                  (b/insert-char \b)
+                  (b/insert-char \c)
+                  (b/move-point-backward)
+                  (core/kill-line))]
+      (is (= "ab" (:text buf))))))
