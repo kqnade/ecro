df88c7ba1964353bab794dcefd8b3e75002cb799
kqnade <git@k4na.de>
2026-06-10 17:47:13 +0900

    ✨ feat: add file I/O with find-file and save-buffer commands

---
+++ src/ecro/file.clj
@@ -0,0 +1,32 @@
+(ns ecro.file
+  (:require [ecro.buffer :as b]
+            [clojure.java.io :as io]))
+
+(defn read-file
+  "Read a file into a buffer. Returns empty buffer if file doesn't exist."
+  [filepath]
+  (let [file (io/file filepath)]
+    (if (.exists file)
+      (assoc (b/make-buffer (.getName file))
+             :text (slurp file)
+             :filepath filepath)
+      (assoc (b/make-buffer (.getName file))
+             :text ""
+             :filepath filepath))))
+
+(defn write-file
+  "Write buffer content to its filepath. Returns nil if no filepath."
+  [buf]
+  (when-let [filepath (:filepath buf)]
+    (spit filepath (:text buf))
+    buf))
+
+(defn find-file
+  "Command: find-file (C-x C-f). Read a file into a new buffer."
+  [filepath]
+  (read-file filepath))
+
+(defn save-buffer
+  "Command: save-buffer (C-x C-s). Write current buffer to file."
+  [buf]
+  (write-file buf))
---
+++ test/ecro/file_test.clj
@@ -0,0 +1,61 @@
+(ns ecro.file-test
+  (:require [clojure.test :refer :all]
+            [ecro.file :as f]
+            [clojure.java.io :as io]))
+
+(deftest test-read-file
+  (testing "reading a file into a buffer"
+    (let [test-file (str (System/getProperty "java.io.tmpdir") "/ecro_test_" (System/currentTimeMillis) ".txt")]
+      (try
+        (spit test-file "hello world")
+        (let [buf (f/read-file test-file)]
+          (is (= "hello world" (:text buf)))
+          (is (= test-file (:filepath buf))))
+        (finally
+          (io/delete-file test-file true))))))
+
+(deftest test-write-file
+  (testing "writing a buffer to a file"
+    (let [test-file (str (System/getProperty "java.io.tmpdir") "/ecro_test_" (System/currentTimeMillis) ".txt")]
+      (try
+        (let [buf {:name "test"
+                   :text "hello ecro"
+                   :filepath test-file}]
+          (f/write-file buf)
+          (is (= "hello ecro" (slurp test-file))))
+        (finally
+          (io/delete-file test-file true))))))
+
+(deftest test-find-file-command
+  (testing "find-file creates a buffer from file"
+    (let [test-file (str (System/getProperty "java.io.tmpdir") "/ecro_test_" (System/currentTimeMillis) ".txt")]
+      (try
+        (spit test-file "test content")
+        (let [result (f/find-file test-file)]
+          (is (= "test content" (:text result)))
+          (is (= test-file (:filepath result))))
+        (finally
+          (io/delete-file test-file true))))))
+
+(deftest test-save-buffer-command
+  (testing "save-buffer writes buffer to its filepath"
+    (let [test-file (str (System/getProperty "java.io.tmpdir") "/ecro_test_" (System/currentTimeMillis) ".txt")]
+      (try
+        (let [buf {:name "test"
+                   :text "saved content"
+                   :filepath test-file}]
+          (f/save-buffer buf)
+          (is (= "saved content" (slurp test-file))))
+        (finally
+          (io/delete-file test-file true))))))
+
+(deftest test-read-nonexistent-file
+  (testing "reading nonexistent file creates empty buffer"
+    (let [buf (f/read-file "/tmp/nonexistent_ecro_file.txt")]
+      (is (= "" (:text buf))))))
+
+(deftest test-write-buffer-without-path
+  (testing "writing buffer without filepath returns nil"
+    (let [buf {:name "test"
+               :text "no path"}]
+      (is (nil? (f/write-file buf))))))
