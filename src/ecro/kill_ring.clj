c2085d8fc22bea3ac9ccc6fdf99f1fd5f1d5f9b6
kqnade <git@k4na.de>
2026-06-10 20:39:04 +0900

    ✨ feat: add kill-ring with yank, yank-pop, kill-line, kill-region

---
+++ src/ecro/kill_ring.clj
@@ -0,0 +1,72 @@
+(ns ecro.kill-ring
+  (:require
+    [ecro.buffer :as b]))
+
+
+(defn make-kill-ring
+  "Create a new kill ring with optional max size."
+  ([] (make-kill-ring 60))
+  ([max-size]
+   {:entries []
+    :index 0
+    :max-size max-size}))
+
+
+(defn kill-text
+  "Add text to the kill ring."
+  [kr text]
+  (if (seq text)
+    (let [entries (vec (take (:max-size kr) (cons text (:entries kr))))]
+      (assoc kr :entries entries :index 0))
+    kr))
+
+
+(defn kill-append
+  "Append text to the last kill."
+  [kr text]
+  (if (and (seq text) (seq (:entries kr)))
+    (let [last-entry (first (:entries kr))
+          new-entry (str last-entry text)]
+      (-> kr
+          (update :entries rest)
+          (kill-text new-entry)))
+    (kill-text kr text)))
+
+
+(defn yank
+  "Return the current entry from the kill ring."
+  [kr]
+  (when (seq (:entries kr))
+    (nth (:entries kr) (:index kr) "")))
+
+
+(defn yank-pop
+  "Cycle to the previous entry in the kill ring."
+  [kr]
+  (if (seq (:entries kr))
+    (let [new-index (mod (inc (:index kr)) (count (:entries kr)))]
+      (assoc kr :index new-index))
+    kr))
+
+
+(defn kill-line
+  "Kill from point to end of line. Returns [new-buffer killed-text]."
+  [buf]
+  (let [point (:point buf)
+        text (:text buf)
+        next-newline (clojure.string/index-of text "\n" point)
+        end (or next-newline (count text))
+        killed (subs text point end)
+        new-text (str (subs text 0 point) (subs text end))]
+    [(assoc buf :text new-text) killed]))
+
+
+(defn kill-region
+  "Kill text between mark and point. Returns [new-buffer killed-text]."
+  [buf mark point]
+  (let [start (min mark point)
+        end (max mark point)
+        text (:text buf)
+        killed (subs text start end)
+        new-text (str (subs text 0 start) (subs text end))]
+    [(assoc buf :text new-text :point start) killed]))
---
+++ test/ecro/kill_ring_test.clj
@@ -0,0 +1,103 @@
+(ns ecro.kill-ring-test
+  (:require
+    [clojure.test :refer :all]
+    [ecro.buffer :as b]
+    [ecro.kill-ring :as kr]))
+
+
+(deftest test-make-kill-ring
+  (testing "kill ring starts empty"
+    (let [kr (kr/make-kill-ring)]
+      (is (empty? (:entries kr)))
+      (is (= 0 (:index kr))))))
+
+
+(deftest test-kill-text
+  (testing "adding text to kill ring"
+    (let [kr (-> (kr/make-kill-ring)
+                 (kr/kill-text "hello"))]
+      (is (= ["hello"] (:entries kr)))
+      (is (= 0 (:index kr))))))
+
+
+(deftest test-kill-append
+  (testing "appending to previous kill"
+    (let [kr (-> (kr/make-kill-ring)
+                 (kr/kill-text "hello")
+                 (kr/kill-append " world"))]
+      (is (= ["hello world"] (:entries kr))))))
+
+
+(deftest test-yank
+  (testing "yank returns last killed text"
+    (let [kr (-> (kr/make-kill-ring)
+                 (kr/kill-text "hello")
+                 (kr/kill-text "world"))]
+      (is (= "world" (kr/yank kr))))))
+
+
+(deftest test-yank-pop
+  (testing "yank-pop cycles through kill ring"
+    (let [kr (-> (kr/make-kill-ring)
+                 (kr/kill-text "first")
+                 (kr/kill-text "second")
+                 (kr/kill-text "third"))]
+      (is (= "third" (kr/yank kr)))
+      (let [kr2 (kr/yank-pop kr)]
+        (is (= "second" (kr/yank kr2)))
+        (let [kr3 (kr/yank-pop kr2)]
+          (is (= "first" (kr/yank kr3)))
+          (let [kr4 (kr/yank-pop kr3)]
+            (is (= "third" (kr/yank kr4)))))))))
+
+
+(deftest test-kill-line
+  (testing "kill-line extracts text from point to end"
+    (let [buf (-> (b/make-buffer "test")
+                  (b/insert-char \h)
+                  (b/insert-char \e)
+                  (b/insert-char \l)
+                  (b/insert-char \l)
+                  (b/insert-char \o)
+                  (b/insert-char \space)
+                  (b/insert-char \w)
+                  (b/insert-char \o)
+                  (b/insert-char \r)
+                  (b/insert-char \l)
+                  (b/insert-char \d)
+                  (b/move-point-backward)
+                  (b/move-point-backward)
+                  (b/move-point-backward)
+                  (b/move-point-backward)
+                  (b/move-point-backward)
+                  (b/move-point-backward))
+          [new-buf killed] (kr/kill-line buf)]
+      (is (= "hello" (:text new-buf)))
+      (is (= " world" killed)))))
+
+
+(deftest test-kill-region
+  (testing "kill-region extracts text between mark and point"
+    (let [buf (-> (b/make-buffer "test")
+                  (b/insert-char \h)
+                  (b/insert-char \e)
+                  (b/insert-char \l)
+                  (b/insert-char \l)
+                  (b/insert-char \o))
+          ;; mark at 1, point at 4
+          mark 1
+          point 4
+          [new-buf killed] (kr/kill-region buf mark point)]
+      (is (= "ho" (:text new-buf)))
+      (is (= "ell" killed)))))
+
+
+(deftest test-kill-ring-max
+  (testing "kill ring respects max size"
+    (let [kr (-> (kr/make-kill-ring 2)
+                 (kr/kill-text "one")
+                 (kr/kill-text "two")
+                 (kr/kill-text "three"))]
+      (is (= 2 (count (:entries kr))))
+      (is (= "three" (first (:entries kr))))
+      (is (= "two" (second (:entries kr)))))))
