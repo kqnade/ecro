e5e793ad36d493efe83c09e65675992abfd37356
kqnade <git@k4na.de>
2026-06-10 16:23:28 +0900

    🔧 chore: add cbindgen for C header generation

---
+++ rust/ecro-core/ecro_core.h
@@ -0,0 +1,13 @@
+#include <cstdarg>
+#include <cstdint>
+#include <cstdlib>
+#include <ostream>
+#include <new>
+
+extern "C" {
+
+int32_t ecro_init();
+
+int32_t ecro_shutdown();
+
+}  // extern "C"
